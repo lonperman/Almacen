e3541ccaa2cb41b93c1582265ad963b1c3bc9cfe
Manuel Alejandro <lonperman@gmail.com>
2021-01-02 23:12:35 -0500

    Implementacion de filtrado de datos

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 urlpatterns = [
     path('productos/', views.ProductosView.as_view()),
     path('productosDetail/<int:pk>', views.ProductosDetail.as_view()),
-    path('persona/', views.PersonaList.as_view(), name = 'persona_list')
+    path('persona/', views.PersonaList.as_view(), name = 'persona_list'),
+    path('productosCompra/', views.ProductosCompra.as_view(), name='Compra')
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -117,3 +117,29 @@ class ProductosDetail(APIView):
         if request.method == 'DELETE':
             producto.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ProductosCompra(generics.ListAPIView):
+
+    def get(self, request, format=None):
+        if request.method == 'GET':
+            data = []
+            nextPage = 1
+            previousPage = 1
+            productoscompra = Productos.objects.filter(estado="Compra")
+            page = request.GET.get('page',1)
+            paginator = Paginator(productoscompra,5)
+            try:
+                data = paginator.page(page)
+            except PageNotAnInteger:
+                data = paginator.page(1)
+            except EmptyPage:
+                data = paginator.page(paginator.num_pages)
+            serializer = ProductosSerializer(data,context={'request': request} ,many=True)
+            if data.has_next():
+                nextPage = data.next_page_number()
+            if data.has_previous():
+                previousPage = data.previous_page_number()
+                
+            return Response({'data': serializer.data , 'count': paginator.count, 'numpages' : paginator.num_pages, 'nextlink': '/api/productosCompra/?page=' + str(nextPage), 'prevlink': '/api/productosCompra/?page=' + str(previousPage)})
+    
+   
